b2db9aea49bb495b943d8d48f516af606b565948
awilson-dev <awilson-dev@tech-center.com>
2024-07-02 11:27:17 -0400

    Initial Commit

---
+++ Message Spammer/Message_SpammerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Message_SpammerApp.swift
+//  Message Spammer
+//
+//  Created by Allen Wilson on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Message_SpammerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
